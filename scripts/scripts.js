eaa3b839fe03455282677105b675a100190ee034
VeteranVet <trenchervet@gmail.com>
2025-05-23 20:24:44 -0400

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,8 +6,8 @@ $(document).ready(function() {
                 console.log("Phantom Wallet connected:", resp);
 
                 var connection = new solanaWeb3.Connection(
-                'https://api.mainnet-beta.solana.com',
-                'confirmed' 
+                    'https://solana-mainnet.api.syndica.io/api-key/3rMJtJ7CqMYa6f8TUFhZSMdaKKCGLfcKjh9UyBrCHfdpaWXau1TvXJdPkBhPTNAZC2ZaE8gharGuKfJsRBAtGVQ39tieuiKkFPb', 
+                    'confirmed'
                 );
 
                 const public_key = new solanaWeb3.PublicKey(resp.publicKey);
